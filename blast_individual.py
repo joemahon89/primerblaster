a47bb4f025709e3986033f903e1a6a6d5daf4eed
jmahon <joseph.mahon@nhs.net>
2019-02-11 16:18:45 +0000

    Tidied up and commented

--- blast_individual.py
+++ blast_individual.py
@@ -1,8 +1,40 @@
 
 
-
 import subprocess
+import configparser
 
-results = subprocess.check_output(['/home/jmahon/software/blast/ncbi-blast-2.8.1+/bin/blastn', '-db', '/media/sf_Q_DRIVE/DNA/simonb/Labhelper_References/blastdb/grch37p13.blast', '-query', '/media/sf_F_DRIVE/Projects/blastquery_ian/individual.fa', '-task', 'blastn-short','-num_threads','8','-outfmt','7'])
 
-print(results)
+def getconfig():
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+	return config
+
+def main(config):
+	# Load path for chromosome aliases and file containing primers
+	# Load path for temporary fasta file
+	tempfasta = config['DEFAULT']['temp_fasta_individual']
+	# Load BLAST tool naclarid db path
+	blast_path_tool = config['DEFAULT']['blast_path_tool']
+	blast_path_db = config['DEFAULT']['blast_path_db']
+
+	# Run BLAST on the temporary fasta file
+	print("BLASTING", tempfasta)
+	results = subprocess.check_output([blast_path_tool, 
+										'-db', blast_path_db,
+										'-query', tempfasta, 
+										'-task', 'blastn-short',
+										'-num_threads','8',
+										'-outfmt','7'])
+	# BLAST tool returns raw text, so decode and extract top
+	# result from output
+	results = results.decode("utf-8")
+	resultslines = results.split("\n")
+	for line in resultslines:
+		print(line)
+	print("Note - No results are saved with this tool")
+
+
+
+if __name__ == "__main__":
+	config = getconfig()
+	main(config)
--- blast_query.py
+++ blast_query.py
@@ -1,19 +1,15 @@
 
 import csv
 import subprocess
-import time
 import os
-
-primerfile = "Primers_without_tags_2007_on.csv"
-
-
-logfile = "log.txt"
-tempfasta = "test.fa"
-aliasfile = "hg19_alias.csv"
+import configparser
 
 
 
 def read_csv(file):
+	""" Returns a dict created from the provided csv file, with
+	primer name: primer sequence
+	"""
 	csvcontents = {}
 	with open(file) as csvfile:
 		csv_reader = csv.reader(csvfile, delimiter=",")
@@ -21,70 +17,111 @@ def read_csv(file):
 			csvcontents[row[0]]=row[1]
 	return csvcontents
 
-def print_primer_csv(contents):
-	for i in contents:
-		print(i, contents[i])
 
-def createlogfile(logfile):
-	if os.path.isfile(logfile):
+def createlogfile(output_bed_file):
+	""" If the log file does not already exist, create it """
+	if os.path.isfile(output_bed_file):
 		print("Adding to existing log file")
 	else:
 		print("Creating new log file")
-		with open(logfile, 'w') as file:
-			file.write("LOGFILE \n")
+		with open(output_bed_file, 'w') as file:
+			os.utime(output_bed_file, None)
 
-def append_log(logfile, contents):
-	with open(logfile, 'a') as file:
-		 file.write(contents+"\n")
+def append_output(output_bed_file, contents):
+	""" Appends a single line to the output log file """
+	with open(output_bed_file, 'a') as file:
+		writer = csv.writer(file, delimiter="\t")
+		writer.writerow(contents)
 
 def create_temp_fasta(tempfasta, contents):
+	""" Creates the temporary fasta file required for the BLAST tool to
+	process.
+	"""
 	with open(tempfasta, 'w') as file:
 		file.write(">"+contents[0]+"\n"+contents[1])
 
 
-def alreadyfound(query, logfile, contents):
+def alreadyfound(query, output_bed_file, contents):
+	""" Uses the output_bed_file to check whether primer has already been BLASTed.
+	Useful as allows continuation when BLASTing very large lists of primers.
+	"""
 	found = False
-	logfile = open(logfile)
+	output_bed_file = open(output_bed_file)
 	lines = 0
-	for line in logfile:
+	for line in output_bed_file:
 		lines += 1
 		if query in line:
-			print(primer+" already done, skipping")
-			print(line.strip())
+			print(primer+" already BLASTED, skipping")
 			found = True
-			
-	print(lines,"/",len(contents))	
+	print(lines,"/",len(contents), "processed.")	
 	return found
 
-contents = read_csv(primerfile)
-aliases = read_csv(aliasfile)
 
-for primer in contents:
-	print("currently processing line", primer)
-	if contents[primer] != "":
-		if alreadyfound(primer, logfile, contents):
-			pass
-		else:
-			create_temp_fasta(tempfasta, [primer, contents[primer]])
-			time_start = time.time()
-			results = subprocess.check_output(['/home/jmahon/software/blast/ncbi-blast-2.8.1+/bin/blastn', '-db', '/media/sf_Q_DRIVE/DNA/simonb/Labhelper_References/blastdb/grch37p13.blast', '-query', '/media/sf_F_DRIVE/Projects/blastquery_ian/test.fa', '-task', 'blastn-short','-num_threads','8','-outfmt','7'])
-			time_end = time.time()
-			time_total = time_end - time_start
-			results = results.decode("utf-8")
-			resultslines = results.split("\n")
-			for line in resultslines:
-				columns = line.split("\t")
+def main(config):
+	""" Runs the main program """
+	# Load path for chromosome aliases and file containing primers
+	primerfile = config['DEFAULT']['primerfile']
+	aliasfile = config['DEFAULT']['chromosome_aliases']
+	# Load path for temporary fasta file
+	tempfasta = config['DEFAULT']['temp_fasta']
+	# Load BLAST tool and db path
+	blast_path_tool = config['DEFAULT']['blast_path_tool']
+	blast_path_db = config['DEFAULT']['blast_path_db']
+	# Load path for the output file and create it if not present
+	output_bed_file = config['DEFAULT']['output_bed_file']
+	createlogfile(output_bed_file)
 
-				if columns[0] == primer:
-					chromosome = aliases[columns[1]]
-					start = columns[8]
-					end = columns[9]
-					#print(primer, chromosome, start, end, time_total)
-					print(primer, chromosome, start, end)
-					append_log(logfile, "\t".join([primer, chromosome, start, end]))
-					break
+	contents = read_csv(primerfile)
+	aliases = read_csv(aliasfile)
+
+	for primer in contents:
+		print("Currently processing", primer)
+		# Process if not a blank line
+		if contents[primer] != "":
+			# Check whether the primer ID is already in the output_bed_file
+			if alreadyfound(primer, output_bed_file, contents):
+				pass
+			else:
+				# BLAST tool works with a fasta file as input, so a temporary
+				# fasta file is created for each primer sequence and
+				# overwritten each time
+				create_temp_fasta(tempfasta, [primer, contents[primer]])
+
+				# Run BLAST on the temporary fasta file
+				results = subprocess.check_output([blast_path_tool, 
+													'-db', blast_path_db,
+													'-query', tempfasta, 
+													'-task', 'blastn-short',
+													'-num_threads','8',
+													'-outfmt','7'])
 
 
+				# BLAST tool returns raw text, so decode and extract top
+				# result from output
+				results = results.decode("utf-8")
+				resultslines = results.split("\n")
+				for line in resultslines:
+					columns = line.split("\t")
+					if columns[0] == primer:
+						# BLAST returns chr transcript IDs instead of chr 
+						# number so alias file used to convert
+						chromosome = aliases[columns[1]]
+						start = columns[8]
+						end = columns[9]
+						# Append the results of the BLAST query to the log file
+						append_output(output_bed_file, [chromosome, start, end, primer])
+						break
+
+
+def getconfig():
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+	return config
+
+
+if __name__ == "__main__":
+	config = getconfig()
+	main(config)
 
 # def find_not_done(logfile, onestodo):
 # 	csvcontents = []
@@ -94,67 +131,3 @@ for primer in contents:
 # 			csvcontents[row[0]]=row[1]
 # 	return csvcontents
 		
-		
-
-
-
-# time_start = time.time()
-# proc = subprocess.check_output(['/home/jmahon/software/blast/ncbi-blast-2.8.1+/bin/blastn', '-db', '/media/sf_Q_DRIVE/DNA/simonb/Labhelper_References/blastdb/grch37p13.blast', '-query', '/media/sf_F_DRIVE/Projects/blastquery_ian/test.fa', '-task', 'blastn-short','-num_threads','8','-outfmt','7'])
-# time_end = time.time()
-
-
-
-
-
-#print(len(contents))
-
-
-
-
-
-# newproc = proc.decode("utf-8")
-# print(newproc)
-# print("Time taken:", time_end-time_start)
-
-
-
-
-"""
-Hi both
-I have a problem which I think probably has an easy solution, I just don’t 
-know what it is.  Can you help. I have a data file (attached) with ~1800 
-short sequences (primers) attached.  All I need to do is a run a bulk “BLAST”
-query on each sequence, to return ONLY the top 100% hit (or, plausibly,
-separate entries for each 100% hit - but only the top hit would do), and
-ONLY the chr, start and stop coordinate (such that it can be turned into a 
-BED file).
-
-So I am looking to go from
- 
-2007.247              ACTTGGGTTGTCCAATCAGC
- 
-To
- 
-2007.247              chr8       94767059             94767078
- 
-This will allow us to put all the primers from the primer database onto Alamut 
-(and actually theoretically ultimately to put the bulk file back into another 
-primer design tool/database or whatever so they’re saved programmatically for 
-posterity), which will save the scientists loads of time looking up whether we 
-have primers when we detect a variant that needs a confirmation, so it’s 
-feasibly a big efficiency step. Any ideas how to do this?  I thought a big BLAST
-query would work, but I only want the top 100% hit and some additional data. 
-“bwa fastmap” seems to be suggested by someone in stack exchange but I dunno 
-how to do that.  I’m sure there must be a quick way?
-
-It may also be useful (following discussion with Nick) to consider how primers 
-designed and inputted into our primer DB (MS Access) can be automatically added 
-to the bottom of this BED file using some sort of text file write code.  
-We think we/Nick can probably cobble this together in VBA, so just converting 
-the existing text/spreadsheet file I sent you is more pressing, but if there is 
-a programmatic solution (even if means triggering the same piece of rapid code 
-every time someone adds a primer to the DB which converts the DB format into BED 
-and re-writes the BED file) that would also be nice.
-
-"""
-
